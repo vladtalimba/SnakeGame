f8a4e02bc92b6e05ef046f8819ddc60b5bb93f49
Vlad Talimba <talimba_vlad@yahoo.com>
2024-07-09 17:23:56 +0300

    initial commit

---
+++ SnakeGame/Interfaces/IFood.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame.Interfaces
+{
+    internal interface IFood
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        void Spawn();
+        void HoldPosition();
+        void Die();
+    }
+}
---
+++ SnakeGame/Interfaces/ISnake.cs
@@ -0,0 +1,18 @@
+﻿using SnakeGame.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame.Interfaces
+{
+    internal interface ISnake
+    {
+        List<Positions> SnakeBody { get; }
+        void Move();
+        void Die();
+        void Eat();
+        void Spawn();
+    }
+}
